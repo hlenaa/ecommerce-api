ef936a74ee3b6487ab40194b630575d5b7a4b72b
Jackie <jacquelinederigs@gmail.com>
2025-03-08 15:40:44 +0100

    updates to branch jackie

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import express from "express"
+import express from "express";
 //import errorHandler
+import errorHandler from "./middleware/errorHandler.js";
 //import router Files
+import categoryRouter from "./routers/categoryRouter.js";
 
 const app = express();
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 //add routes + routers
-app.use("/users");
-app.use("/products");
-app.use("/categories");
-app.use("/orders");
+app.use("/categories", categoryRouter);
+
 
 // add ErrorHandler inside
-// app.use(); 
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
