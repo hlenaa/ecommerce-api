f1006893c3700108b56fd4d4884e8d84c82aef04
Lena Hauer <lena.hauer@me.com>
2025-03-08 11:43:58 +0100

    Updated associations

--- src/db/associations.js
+++ src/db/associations.js
@@ -1,8 +1,14 @@
 import sequelize from "./index.js";
 import Category from "../models/Category.js";
+import Product from "../models/Product.js";
+import Order from "../models/Order.js";
+import OrderItem from "../models/OrderItem.js";
 
 //Category associations (One to Many)
 Category.hasMany(Product, { foreignKey: "categoryId" }); //category has many products
 Product.belongsTo(Category, { foreignKey: "categoryId" }); // product belongs to one category
+
+Order.belongsToMany(Product, { through: OrderItem, foreignKey: 'orderId' });
+Product.belongsToMany(Order, { through: OrderItem, foreignKey: 'productId' });
 
 sequelize.sync(); //create the tables if they do not exist
