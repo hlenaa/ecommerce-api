77251c46bae89a7605b75c7edd1db98c06650615
Lena Hauer <lena.hauer@me.com>
2025-03-08 14:58:40 +0100

    Adjusted models and added schemaCheck

---
+++ src/middleware/schemaCheck.js
@@ -0,0 +1,10 @@
+import ExtendedError from "../utils/ExtendedError.js";
+
+export const schemaChecker = (schema) => async (req, res, next) => {
+    try {
+      await schema.validateAsync(req.body);
+      next();
+    } catch (err) {
+      next(new ExtendedError(400, err.message));
+    }
+};
--- src/models/Category.js
+++ src/models/Category.js
@@ -3,9 +3,7 @@ import sequelize from "../db/index.js";
 
 //model
 const Category = sequelize.define("Category", {
-name: {type: DataTypes.STRING, allowNull: false},
+name: {type: DataTypes.STRING, allowNull: false, unique: true},
 });
-
-Category.sync(); //create the table if it does not exist
 
 export default Category;
