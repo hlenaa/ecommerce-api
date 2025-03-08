cfb06b44fda7bc0400111c1e5026d8955eb55ba5
Lena Hauer <lena.hauer@me.com>
2025-03-08 12:04:36 +0100

    Use automatically created id for order files

--- src/models/Order.js
+++ src/models/Order.js
@@ -2,11 +2,6 @@ import { DataTypes } from 'sequelize';
 import sequelize from '../db/index.js';
 
 const Order = sequelize.define('Order', {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    },
     userId: {
         type: DataTypes.INTEGER,
         allowNull: false,
