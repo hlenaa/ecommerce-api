197175371038dc83c6ea19f67a1a16a690ad3a32
Jackie <jacquelinederigs@gmail.com>
2025-03-15 11:04:48 +0100

    updated Category Id Check

--- src/middleware/schemaCheck.js
+++ src/middleware/schemaCheck.js
@@ -1,12 +1,12 @@
 import ExtendedError from "../utils/ExtendedError.js";
 
 export const schemaChecker = (schema) => async (req, res, next) => {
-    try {
-      await schema.validateAsync(req.body);
-      next();
-    } catch (err) {
-      next(new ExtendedError(400, err.message));
-    }
+	try {
+		await schema.validateAsync(req.body);
+		next();
+	} catch (err) {
+		next(new ExtendedError(400, err.message));
+	}
 };
 
 export default schemaChecker;
